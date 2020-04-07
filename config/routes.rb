c8c183634a38e526695114d1e6ecdb534e88ae9c
Klaudia <klau.sobotka@gmail.com>
2020-04-07 13:05:52 +0200

    Add 'My houses' view.

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'houses#index'
   resources :houses
+  get 'myhouses', to: 'houses#index_owner'
 
   resources :conversations do
     resources :messages
