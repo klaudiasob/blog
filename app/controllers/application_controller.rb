51f44c4685a83bc40d2513f341a3d6a69da8a61d
klaudiasob <61837479+klaudiasob@users.noreply.github.com>
2020-04-24 10:47:07 +0200

    Add possibility to change password. (#19)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,11 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  def after_sign_in_path_for(resource)
-    stored_location_for(resource) || root_path
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    update_attrs = [:password, :password_confirmation, :current_password]
+    devise_parameter_sanitizer.permit :account_update, keys: update_attrs
   end
 
   def current_ability
